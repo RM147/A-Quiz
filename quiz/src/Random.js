ef26836f4d9f7cfabff597f486bedf67615ca85c
RM147 <mittrarana@gmail.com>
2019-04-25 15:58:11 +0100

    added limited question functions

--- quiz/src/Random.js
+++ quiz/src/Random.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import * as QB from './QuestionBankPokemon.js';
 import * as Constants from './Constants.js';
+import Button from './Button.js';
 
 class Random extends Component {
 
@@ -11,7 +12,8 @@ class Random extends Component {
             value: '',
             answer: '',
             rand: (Math.floor(Math.random() * (this.props.highEnd-this.props.lowEnd))+this.props.lowEnd),
-            score: 0
+            score: 0,
+            questions: ''
         };
     }
 
@@ -30,16 +32,42 @@ class Random extends Component {
 
     }
 
+    changeQuestions1 = () => {
+        this.setState({ questions: ''});
+
+    }
+
+    changeQuestions2 = () => {
+        this.setState({ questions: 10});
+
+    }
+
+    changeQuestions3 = () => {
+        this.setState({ questions: 25});
+
+    }
+
+    changeQuestions4 = () => {
+        this.setState({ questions: 50});
+
+    }
+
     render() {
         return (
             <div>
+                <div className="grid2">
+                <Button text="Endlessmode" onClick={this.changeQuestions1}/>
+                <Button text="10 Questions" onClick={this.changeQuestions2}/>
+                <Button text="25 Questions" onClick={this.changeQuestions3}/>
+                <Button text="50 Questions" onClick={this.changeQuestions4}/>
+                </div>
                 
             <h1>{this.state.rand}</h1>            
             <img src={"http://www.pokestadium.com/assets/img/sprites/"+this.state.rand+".png"}/>
             <div>Answer: <input type="text" onChange={this.changeValue}/></div>
             <button onClick={this.answerMatch}>Submit</button>
             <p>{this.state.answer}</p>
-            <p>Score: {this.state.score}</p>
+            <p>Score: {this.state.score}/{this.state.questions}</p>
             <p>Answers must begin with a capital letter</p>
             </div>
 
