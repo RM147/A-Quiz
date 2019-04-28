43f04d7da7d5da07532252ff14b8cf5323adcb36
RM147 <mittrarana@gmail.com>
2019-04-28 17:46:22 +0100

    fixed edge case issue

--- quiz/src/Constants.js
+++ quiz/src/Constants.js
@@ -1,7 +1,7 @@
 export const gen0 = 0
-export const gen1 = 151
-export const gen2 = 251
-export const gen3 = 386
-export const gen4 = 493
-export const gen5 = 649
-export const gen6 = 721
+export const gen1 = 152
+export const gen2 = 252
+export const gen3 = 387
+export const gen4 = 494
+export const gen5 = 650
+export const gen6 = 722
