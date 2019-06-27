eead02f1c92faed729bf71eecd860daf92170000
RM147 <mittrarana@gmail.com>
2019-06-27 15:04:01 +0100

    updated hardcore mode
    Removed range control - generations only for now. This is for making a working version with no half done features for merging with master branch.

--- quiz/src/Dropdown.js
+++ quiz/src/Dropdown.js
@@ -5,8 +5,7 @@ class Dropdown extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Range: {this.props.low}-{this.props.high - 1}</h1>
+            <div>              
 
             </div>
 
